03cffaef3f4aa81de76daab74309d6f925905e99
Chris Nelson <superchrisnelson@gmail.com>
2025-08-27 14:23:03 -0400

    need import defined or gotta restart

--- lib/wasm_commerce/orders/shipping_calculator.ex
+++ lib/wasm_commerce/orders/shipping_calculator.ex
@@ -1,7 +1,17 @@
 defmodule WasmCommerce.Orders.ShippingCalculator do
   use Wasmex.Components.ComponentServer,
     wit: "wasm/shipping-calculator.wit",
-    imports: %{}
+    imports: %{
+      "product-surcharge" => {:fn, &get_product_surcharge/1}
+    }
 
   def calculate_shipping(order), do: calculate_shipping(__MODULE__, order)
+
+  def get_product_surcharge(%{name: product_name}) do
+    if String.contains?(product_name, "Premium") do
+      500
+    else
+      0
+    end
+  end
 end
